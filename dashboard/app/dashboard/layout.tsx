cd1acc05d2c1b0b2962792831f040b83dbbe0a9b
Krishna Tiwari <tiwarikrishna54321@gmail.com>
2026-03-15 22:34:37 +0545

    dashboard layout basics

---
+++ dashboard/app/dashboard/layout.tsx
@@ -0,0 +1,275 @@
+import React from "react"
+
+import ProfileDropdown from "@/components/dropdown-profile"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import {
+  ArrowRightLeftIcon,
+  CalendarClockIcon,
+  ChartNoAxesCombinedIcon,
+  ChartPieIcon,
+  ChartSplineIcon,
+  ClipboardListIcon,
+  Clock9Icon,
+  CrownIcon,
+  Facebook,
+  HashIcon,
+  Instagram,
+  Linkedin,
+  SettingsIcon,
+  SquareActivityIcon,
+  Twitter,
+  Undo2Icon,
+  UsersIcon,
+} from "lucide-react"
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <section className="flex min-h-dvh w-full">
+      <SidebarProvider>
+        <Sidebar>
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <ChartNoAxesCombinedIcon />
+                        <span>Dashboard</span>
+                      </a>
+                    </SidebarMenuButton>
+                    <SidebarMenuBadge className="rounded-full bg-primary/10">
+                      5
+                    </SidebarMenuBadge>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+            <SidebarGroup>
+              <SidebarGroupLabel>Pages</SidebarGroupLabel>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <ChartSplineIcon />
+                        <span>Content Performance</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <UsersIcon />
+                        <span>Audience Insight</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <ChartPieIcon />
+                        <span>Engagement Metrics</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <HashIcon />
+                        <span>Hashtag Performance</span>
+                      </a>
+                    </SidebarMenuButton>
+                    <SidebarMenuBadge className="rounded-full bg-primary/10">
+                      3
+                    </SidebarMenuBadge>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <ArrowRightLeftIcon />
+                        <span>Competitor Analysis</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <Clock9Icon />
+                        <span>Campaign Tracking</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <ClipboardListIcon />
+                        <span>Sentiment Tracking</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <CrownIcon />
+                        <span>Influencer</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+            <SidebarGroup>
+              <SidebarGroupLabel>Supporting Features</SidebarGroupLabel>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <SquareActivityIcon />
+                        <span>Real Time Monitoring</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <CalendarClockIcon />
+                        <span>Schedule Post & Calendar</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <Undo2Icon />
+                        <span>Report & Export</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <SettingsIcon />
+                        <span>Settings & Integrations</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">
+                        <UsersIcon />
+                        <span>User Management</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </SidebarContent>
+        </Sidebar>
+        <div className="flex flex-1 flex-col">
+          <header className="sticky top-0 z-50 border-b bg-card">
+            <div className="mx-auto flex max-w-7xl items-center justify-between gap-6 px-4 py-2 sm:px-6">
+              <div className="flex items-center gap-4">
+                <SidebarTrigger className="[&_svg]:!size-5" />
+                <Separator
+                  orientation="vertical"
+                  className="hidden !h-4 sm:block"
+                />
+                <Breadcrumb className="hidden sm:block">
+                  <BreadcrumbList>
+                    <BreadcrumbItem>
+                      <BreadcrumbLink href="#">Home</BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                      <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>Free</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </BreadcrumbList>
+                </Breadcrumb>
+              </div>
+              <div className="flex items-center gap-1.5">
+                {/* <LanguageDropdown
+                  trigger={
+                    <Button variant="ghost" size="icon">
+                      <LanguagesIcon />
+                    </Button>
+                  }
+                /> */}
+                <ProfileDropdown
+                  trigger={
+                    <Button variant="ghost" size="icon" className="size-9.5">
+                      <Avatar className="size-9.5 rounded-md">
+                        <AvatarImage src="https://cdn.shadcnstudio.com/ss-assets/avatar/avatar-1.png" />
+                        <AvatarFallback>JD</AvatarFallback>
+                      </Avatar>
+                    </Button>
+                  }
+                />
+              </div>
+            </div>
+          </header>
+          <main className="size-full flex-1 px-4 py-6 sm:px-6">
+            <Card>
+              <CardContent>{children}</CardContent>
+            </Card>
+          </main>
+          <footer className="flex items-center justify-between gap-3 px-4 py-3 text-muted-foreground max-sm:flex-col sm:gap-6 sm:px-6 md:max-lg:flex-col">
+            <p className="text-center text-sm text-balance">
+              {`©${new Date().getFullYear()}`}{" "}
+              <a href="#" className="text-primary">
+                shadcn/studio
+              </a>
+              , Made for better web design
+            </p>
+            <div className="flex items-center gap-5">
+              <a href="#">
+                <Facebook className="size-4" />
+              </a>
+              <a href="#">
+                <Instagram className="size-4" />
+              </a>
+              <a href="#">
+                <Linkedin className="size-4" />
+              </a>
+              <a href="#">
+                <Twitter className="size-4" />
+              </a>
+            </div>
+          </footer>
+        </div>
+      </SidebarProvider>
+    </section>
+  )
+}
+
+export default DashboardLayout
---
+++ dashboard/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <section className="flex min-h-dvh w-full">check</section>
+}
+
+export default Page
