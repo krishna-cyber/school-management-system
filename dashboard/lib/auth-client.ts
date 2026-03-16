859521a3f5d5ec891cd49479e8ad7ae3af227fef
Krishna Tiwari <tiwarikrishna54321@gmail.com>
2026-03-16 15:15:24 +0545

    protected route dashboard

--- dashboard/lib/auth-client.ts
+++ dashboard/lib/auth-client.ts
@@ -3,13 +3,5 @@ import { createAuthClient } from "better-auth/react" // make sure to import from
 export const authClient = createAuthClient({
   //you can pass client configuration here
   baseURL: "http://localhost:3000",
-  basePath: "/auth",
-  fetchOptions: {
-    onError: (error) => {
-      if (error.response.status === 429) {
-        console.warn("Too many requests. Please try again later.")
-      }
-      console.error("Auth Client Error:", error)
-    },
-  },
+  basePath: "api/auth",
 })
--- dashboard/proxy.ts
+++ dashboard/proxy.ts
@@ -1,18 +1,20 @@
 import { NextRequest, NextResponse } from "next/server"
 import { headers } from "next/headers"
-// import { auth } from "@/lib/auth"
+import { authClient } from "./lib/auth-client"
 
 export async function proxy(request: NextRequest) {
-  // const session = await auth.api.getSession({
-  //   headers: await headers(),
-  // })
+  const { data, error } = await authClient.getSession({
+    fetchOptions: {
+      headers: await headers(),
+    },
+  })
 
-  // // THIS IS NOT SECURE!
-  // // This is the recommended approach to optimistically redirect users
-  // // We recommend handling auth checks in each page/route
-  // if (!session) {
-  //   return NextResponse.redirect(new URL("/sign-in", request.url))
-  // }
+  // THIS IS NOT SECURE!
+  // This is the recommended approach to optimistically redirect users
+  // We recommend handling auth checks in each page/route
+  if (!data) {
+    return NextResponse.redirect(new URL("/sign-in", request.url))
+  }
 
   return NextResponse.next()
 }
